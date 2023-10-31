28a4402a4aec79c24fa481c46eb060920e4d7b86
Alec Mestroni <alec.mestroni@infocert.it>
2023-10-31 15:52:38 +0100

    new control config.awsObj

--- index.js
+++ index.js
@@ -2,34 +2,54 @@ const chalk = require("chalk");
 
 const separator = chalk.white('\n====================================================================================================\n')
 
-module.exports = async (on, config, awsObj) => {
-    if (!awsObj.secretName || !awsObj.profile || !awsObj.region) {
-        console.error(chalk.red('\nConfigurationError:\n') + chalk.yellow('You must specify the "awsObj" element in the config, and it must contain this properties:\n') + chalk.white('secretName, profile, region'))
-        throw new Error('You must specify the "awsObj" element in the config, and it must contain this properties:\n secretName, profile, region')
+module.exports = async (on, config) => {
+    if (config.awsObj) {
+        const awsObj = config.awsObj
+        if (!awsObj.secretName || !awsObj.profile || !awsObj.region) {
+            console.error(chalk.red('\nConfigurationError:\n') + chalk.yellow('You must specify the "awsObj" element in the config, and it must contains this properties: ') + chalk.white('secretName, profile, region\nPassed:' + JSON.stringify(awsObj)))
+            throw new Error('You must specify the "awsObj" element in the config, and it must contain this properties:\n secretName, profile, region')
+        }
+        await loadAwsSecrets(config, awsObj)
     }
-    await loadAwsSecrets(config, awsObj)
     return config;
 };
 
 async function getSecretsFromAws(awsObj) {
     const { SecretsManagerClient, GetSecretValueCommand } = require("@aws-sdk/client-secrets-manager");
     const { fromSSO } = require("@aws-sdk/credential-providers");
-    const client = new SecretsManagerClient({
-        region: awsObj.region,
-        credentials: fromSSO({ profile: awsObj.profile })
-    });
-    const secretName = awsObj.secretName
+    let count = 0
+    let maxTries = 10
     let response;
-    try {
-        response = await client.send(
-            new GetSecretValueCommand({
-                SecretId: secretName,
-                VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-            })
-        );
-    } catch (error) {
-        console.log(error)
-        throw error;
+    let client
+    while (++count <= maxTries) {
+        try {
+            if (!client) {
+                client = new SecretsManagerClient({
+                    region: awsObj.region,
+                    credentials: fromSSO({ profile: awsObj.profile })
+                });
+            }
+            response = await client.send(
+                new GetSecretValueCommand({
+                    SecretId: awsObj.secretName,
+                    VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+                })
+            );
+            break
+        } catch (error) {
+            if (count == 1 && /Profile .*? was not found/gm.test(error.message) && awsObj.profile !== 'default') {
+                console.log(error.message + '\nCredential Error: retring with default profile')
+                awsObj.profile = 'default';
+                client = undefined
+            } else if (count == 2 && /Profile .*? was not found/gm.test(error.message)) {
+                console.log(error.message + '\nCredential Error: retring with unsetted profile')
+                client = new SecretsManagerClient({
+                    region: awsObj.region,
+                })
+            } else {
+                throw error;
+            }
+        }
     }
     const secret = JSON.parse(response.SecretString)
     return secret;
@@ -38,7 +58,7 @@ async function getSecretsFromAws(awsObj) {
 async function loadAwsSecrets(config, awsObj) {
 
     console.log(separator)
-    console.log('Extracting secret from: "' + chalk.cyan('AWS Secrets Manger\n'))
+    console.log('Extracting secret from: "' + chalk.cyan('AWS Secrets Manger"\n'))
     const secret = await getSecretsFromAws(awsObj)
     config.env = {
         ...config.env,
@@ -48,5 +68,5 @@ async function loadAwsSecrets(config, awsObj) {
         secret[key] = "".padStart(5, '*');
     });
     console.log(` - secret: "${JSON.stringify(secret, null, 4)}"`)
-    console.log(chalk.green('\n√ ') + chalk.white('Secret loaded correctly from: "' + chalk.cyan(secret_name) + '"'))
+    console.log(chalk.green('\n√ ') + chalk.white('Secret loaded correctly from: "' + chalk.cyan(awsObj.secretName) + '"'))
 }
